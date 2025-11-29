9515177270b1e2294018a10353e71eaf24ef1f41
Olivier Guizol <oguizol@c1r7p1.42nice.fr>
2025-11-29 17:00:54 +0100

    29112025

--- 03_gnl/get_next_line.c
+++ 03_gnl/get_next_line.c
@@ -6,61 +6,150 @@
 /*   By: oguizol <oguizol@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 15:03:20 by oguizol           #+#    #+#             */
-/*   Updated: 2025/11/28 16:52:48 by oguizol          ###   ########.fr       */
+/*   Updated: 2025/11/29 16:59:17 by oguizol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-t_lstfd	*addnode(t_lstfd *list, int fd)
+int	cutline(t_lstfd *node, char **line)
 {
-	list = (t_lstfd *)malloc(sizeof(t_lstfd));
-	if (!list)
-		return (NULL);
-	list->stash = (char *)malloc (sizeof(char) * (BUFFER_SIZE + 1));
-	if (!(list->stash))
+	int		i;
+
+	i = 0;
+	while (((*line)[i] != '\0') && ((*line)[i] != '\n'))
+		++i;
+	if ((*line)[i] == '\0')
+		return (0);
+	ft_strlcpy((node->stash), (&(*line)[i + 1]), (BUFFER_SIZE + 1));
+	(*line)[i + 1] = '\0';
+	*line = ft_strjoin("", (*line));
+	return (1);
+}
+
+char	*get_line(t_lstfd *node)
+{
+	char	*strj;
+	char	*str;
+	int		len;
+	int		endg;
+
+	endg = 0;
+	strj = NULL;
+	while (!endg)
 	{
-		free (list);
-		return (NULL);
+		str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!str)
+			NULL;
+		len = read(node->fd, str, BUFFER_SIZE);
+		if (len < 0)
+			return (free (str), NULL);
+		str[len] = '\0';
+		if (len < BUFFER_SIZE)
+			node->endofi = 1;
+		str = ft_strjoin(strj, str);
+		strj = ft_strjoin(node->stash, str);
+		*(node->stash) = '\0';
+		endg = cutline(node, &strj);
 	}
-	list->fd = fd;
-	list->endofi = 0;
-	list->next = NULL;
-	return (list);
+	return (strj);
+}
+
+t_lstfd	*getnode(t_lstfd **list, int fd)
+{
+	t_lstfd	*nodefd;
+	t_lstfd	*tmp;
+
+	tmp = (*list);
+	nodefd = (t_lstfd *)malloc(sizeof(t_lstfd));
+	if (!nodefd)
+		return (NULL);
+	nodefd->fd = fd;
+	nodefd->endofi = 0;
+	*(nodefd->stash) = '\0';
+	nodefd->next = NULL;
+	if ((*list) == NULL)
+		*list = nodefd;
+	else
+	{
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = nodefd;
+	}
+	return (nodefd);
+}
+
+void	delnode(t_lstfd **list)
+{
+	free (*list);
+	*list = NULL;
 }
 
 char	*get_next_line(int fd)
 {
 	static t_lstfd	*list;
+	t_lstfd			*nodefd;
 	char			*str;
 
 	str = NULL;
 	if ((fd < 0) || (BUFFER_SIZE < 1))
 		return (NULL);
-	if (!(list))
-		addnode(list, fd);
 	if (!list)
+		nodefd = getnode(&list, fd);
+	else //Juste pour la version sans bonus
+	{
+		nodefd = list;
+		nodefd->fd = fd;
+	}
+	if (!nodefd)
 		return (NULL);
+	str = get_line(nodefd);
+	if ((nodefd->endofi == 1) && (*(nodefd->stash) == '\0'))
+		delnode(&list);
 	return (str);
 }
 
 /*
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
 int	main(void)
 {
 	char	*str;
-	int		fd;
+	int		fd1;
+	//int		fd2;
 
 	str = NULL;
-	fd = open("TEXT1", O_RDONLY);
-	str = get_next_line(fd);
+	fd1 = open("TEXT1", O_RDONLY);
+	//fd2 = open("TEXT2", O_RDONLY);
+	str = get_next_line(fd1);
 	printf("\nTexte recupere:\n%s\n", str);
 	free (str);
-	close (fd);
+	str = get_next_line(fd1);
+	printf("\nTexte recupere:\n%s\n", str);
+	free (str);
+	str = get_next_line(fd1);
+	printf("\nTexte recupere:\n%s\n", str);
+	free (str);
+	str = get_next_line(fd1);
+	printf("\nTexte recupere:\n%s\n", str);
+	free (str);
+
+
+	str = get_next_line(fd2);
+	printf("\nTexte recupere:\n%s\n", str);
+	free (str);
+	str = get_next_line(fd1);
+	printf("\nTexte recupere:\n%s\n", str);
+	free (str);
+	str = get_next_line(fd2);
+	printf("\nTexte recupere:\n%s\n", str);
+	free (str);
+	str = get_next_line(fd1);
+	printf("\nTexte recupere:\n%s\n", str);
+	free (str);
+	str = get_next_line(fd2);
+	printf("\nTexte recupere:\n%s\n", str);
+	free (str);
+	close (fd1);
+	//close (fd2);
 	return (0);
 }
 */
--- 03_gnl/get_next_line.h
+++ 03_gnl/get_next_line.h
@@ -6,28 +6,40 @@
 /*   By: oguizol <oguizol@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 15:03:39 by oguizol           #+#    #+#             */
-/*   Updated: 2025/11/28 16:39:04 by oguizol          ###   ########.fr       */
+/*   Updated: 2025/11/29 16:25:19 by oguizol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 48
+#  define BUFFER_SIZE 20
 # endif
 
 # include <unistd.h>
 # include <stdlib.h>
+/*TEMPO
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+TEMPO END*/
 
 typedef struct s_lstfd
 {
 	int				fd;
-	char			*stash;
+	char			stash[BUFFER_SIZE + 1];
 	int				endofi;
 	struct s_lstfd	*next;
 }							t_lstfd;
 
 char	*get_next_line(int fd);
-t_lstfd	*addnode(t_lstfd *list, int fd);
-
+t_lstfd	*getnode(t_lstfd **list, int fd);
+char	*get_line(t_lstfd *node);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		cutline(t_lstfd *node, char **line);
+void	delnode(t_lstfd **list);
 #endif
